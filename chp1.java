e225eb2b70a6d18488e2616b8d82263c88937155
unknown <furooj0048@gmail.com>
2025-04-01 20:14:37 +0500

    Java Practice

---
+++ chp1.java
@@ -0,0 +1,14 @@
+//Chapter 1 
+// Task 1
+alert("WELCOME")
+// Task 2
+alert("Error! Please Enter a Valid Password")
+// Task 3
+alert("Welcome to JS Land..\n Happy Coding!")
+// Task 4
+alert("Welcome to JS Land..")
+alert("Happy Coding! \n Prevent this page from creating additional dialoges.")
+// Task 5
+alert("Hello.. I can run JS through my web browser’s console")
+var a= "Hello.. I can run JS through my web browser’s console"
+console.log(a)
---
+++ chp2.java
@@ -0,0 +1,30 @@
+alert("Chapter 2 Starts here")
+// task 1
+var username;
+// task 2
+var myname =" Urooj Fatima";
+// task 3
+var message = "Hello World";
+alert(message)
+// tsk 4
+var name ="Jon"
+var age= 15
+var edu="Mobile App Developer"
+alert(name)
+alert(age)
+alert(edu)
+// tsk 5
+var piz="Pizza \n Pizz \n Piz \n Pi \n P"
+alert(piz)
+// task 6
+let e1= "my email address is"
+let e2=" exam@java.com"
+alert(e1 + e2)
+// task 7
+var book = "A smarter way to learn JavaScript"
+alert(book)
+// task 8
+document.write("<h2>Yah! I can write HTML content thru JS</h2>")
+// task 9
+var x="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(x)
