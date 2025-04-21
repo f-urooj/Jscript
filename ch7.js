16088348102fb7248a79cd293063558b7e07fc04
f-urooj <furooj0048@gmail.com>
2025-04-21 17:42:12 +0500

    Add files via upload

---
+++ ch7.js
@@ -0,0 +1,12 @@
+ var calculatedNum1 = 2 + (2 * 6); 
+ console.log(calculatedNum1);
+ var calculatedNum2 = (2 + 2) * 6; 
+ var calculatedNum3 = (2 + 2) * (4 + 2); 
+ var calculatedNum4 = ((2 + 2) * 4) + 2; 
+ console.log(calculatedNum2);
+ console.log(calculatedNum3);
+ console.log(calculatedNum4);
+ 
+ 
+ 
+ 
---
+++ ch8.js
@@ -0,0 +1,14 @@
+ var num = "2" + "2";
+alert(num)
+var message = ("Hello," + "Dolly")
+alert(message)
+ alert("33" + 3)
+ var a= "Pakistan"
+ alert(a + "Zindabad")
+ var x= "hello" + 3
+ console.log(x);
+ var c= "hi"
+ var v= "bye"
+ var b= c+v
+ console.log(b);
+ 
