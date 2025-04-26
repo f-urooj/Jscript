da03d6fe3fa4ae195916a4569335518acde32cd6
f-urooj <furooj0048@gmail.com>
2025-04-26 14:12:12 +0500

    Add files via upload

---
+++ ch21.js
@@ -0,0 +1,58 @@
+var a=20;
+var b= 15;
+if(a>b){
+    alert("hello")
+}
+else {
+    alert("bye")
+}
+    // task 2
+var c=10;
+if (c === 10) {
+        alert("c is 10"); }
+else{
+alert("The correct value of a is")
+}
+// task 3
+var city= prompt("Enter a city:")
+if (city=== "karachi"){
+    alert("You entered Karachi")
+}
+else if(city=== "lahore"){
+    alert("You entered Lahor")
+}
+else{
+        alert("You are wrong")
+}
+// task 4
+var aa, bb, cc, dd;
+if(aa===bb && cc===dd){
+        alert("morning")
+}
+// task 5
+var aaa, b, ccc, ddd;
+if(aaa===bbb || ccc!=ddd){
+        alert(" good morning")
+}
+// 6
+var frst= "urooj"
+var last ="fatima"
+var f= prompt("enter first name")
+var l= prompt("enter last name")
+if(frst==f && last==l){
+    alert("correct")
+}
+else{
+    alert("wrong")
+}
+// ch 15
+var arr=[]
+var ar=["u"]
+var alp=["h","i","j","k",'l','m','n']
+alp.length;
+console.log(length);
+ar.push('r')
+console.log(ar);
+var alpa=["h","i","j","k"]
+alpa.push('l')
+console.log(alpa);
