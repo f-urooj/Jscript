1ea79ff10ad768df3532e96789e247550455b55b
f-urooj <furooj0048@gmail.com>
2025-05-10 08:35:50 +0500

    Add files via upload

--- p1.js
+++ p1.js
@@ -1,121 +1,177 @@
 // 1
-var f=prompt("Enter your first name: ")
-var l=prompt("Enter your last name: ")
-var full= f + l;
-alert("Welcome to our Website Dear " + full)
+var allLower = userInput.toLowerCase();
 // 2
-var m=prompt("Enter your favorite mobile phone model: ")
-var find= m.length;
-alert("Length of string is: "+ find)
+var x=prompt("write smthng: ")
+x=x.toLowerCase();
 // 3
-var pak="Pakistani"
-var fin= pak.indexOf('n');
-alert("index of n is: " + fin)
+var y=prompt("write smthng: ")
+y=y.toUpperCase();
 // 4
-var hel="Hello World"
-var fi= hel.lastIndexOf('l');
-alert("index of last l is: " +fi)
+var a=prompt("write smthng: ")
+var z =a.toLowerCase();
 // 5
-var pa="Pakistani"
-var ff= pa.charAt('3');
-alert("Character at index 3 : " + ff)
+var b="hello";
+alert(b);
+var c=b.toUpperCase();
+alert(c);
 // 6
- var first=prompt("Enter your first name: ")
-var l=prompt("Enter your last name: ")
-var result = first.concat(" ", l)
-alert(result)
-// 7
-var c="Hyderabad"
-var rep= c.replace("Hyder","Islam")
-alert("Before Replacement: " + c)
-alert("After Replacement: " + rep)
-// 8
-var message = "Ali and Sami are best friends. They play cricket and football together"
-var ffind= message.replaceAll("and", "&")
-alert("Before Replacement: " + message)
-alert("After Replacement: " + ffind)
-// 9
-var val= "472";
-alert("value: "+val)
-var ty= typeof(val)
-alert("type: "+ty)
-var val1 = Number(val);
-alert("value: "+val1)
-var ty1= typeof(val1)
-alert("type: "+ty1)
-// 10
- var use=prompt("Enter your first name: ");
- var us1 = use.toUpperCase();
- alert(us1)
-// 11
-var us=prompt("Enter your Last name: ");
-var u1 = us.slice(0,1)
+var city=prompt("Enter your city: ");
+var u1 = city.slice(0,1)
 var u2= u1.toUpperCase();
-var u3= us.replace(u1, u2)
-alert("User input: " + us)
+var u3= city.replace(u1, u2)
+alert("city input: " + city)
 alert("Title Case: " + u3)
+// ch 22
+// 1
+var sameWord= "captain"
+var d= sameWord.slice(1,3)
+alert(d);
+// 2
+var animal= "elephant"
+var seg= animal.slice(2,6)
+alert(seg);
+// 3
+var an= 'abcdefgh'
+var len= an.length;
+// 4
+var text = "To be or not to be.";
+ var indx = text.indexOf("be");
+ console.log(indx);
+// 5
+var text = "To be or not to be.";
+ var indx = text.lastIndexOf("be");
+ console.log(indx);
+//  6
+var text='go'
+var indx=text.lastIndexOf('go');
+// 7
+var indexNum;
+var text;
+if(indexNum>0 && indexNum < text.length){
+
+}
+// 8
+var ch='abcde'
+var findchar= ch.charAt(2)
+console.log(findchar);
+// 9
+var charr= 'abcdefghijklmnop'
+var cha= charr.charAt(10);
+console.log(cha);
+// 10
+var str= 'abcdefghijklmnop'
+str=str.length
+console.log(str);
+var x= str.charAt(16);
+console.log(x);
+// 11
+var input='abcdefghij'
+var cha= input.charAt(5);
+console.log(cha);
 // 12
-var num = 35.36 ;
-var num1= num.toString();
-var num3= num1.replace(".","")
-alert("Number: " + num)
-alert("Result: " + num3)
+if(varr[2]=='c'){
+
+}
 // 13
-var user= prompt("Enter you name: ")
-for (var i = 0; i < user.length; i++) {
-    var cc = username.charCodeAt(i);
-    if(cc==33 || cc==44 || cc==46 || cc==64){
-        // if(user.includes("@")){
-        alert("Please enter valid username")
-        break;
-    }
-    else{
-        alert("username accepted")
-    }
+var arr=[]
+var input=['a','b','c','d']
+for(i=0; i<input.length; i++){
+arr[i]=input[i];
 }
-let username;
-let isValid = false;
-
-while (!isValid) {
-    username = prompt("Enter your username:");
-    isValid = true;
-    for (let i = 0; i < username.length; i++) {
-        let charCode = username.charCodeAt(i);
-
-        if (charCode === 33 || charCode === 44 || charCode === 46 || charCode === 64) {
-            alert("Invalid username! It should not contain any of the following characters: @ . , !");
-            isValid = false;
-            break;
-        }
-    }
-    if (isValid) {
-        alert("Username accepted: " + username);
-    }
-}
+console.log(arr);
 // 14
-var b = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
-var u= prompt("What would you like to have?")
-var bak=u.toLowerCase();
-for(var i=0; i < b.length; i++){
-    if(b.includes(bak)){
-        alert("Please proceed for checkout")
-    }
-    else if(!b.includes(bak)){
-        alert("We are Sorry, we're out of stock")
-    }break;
-}
+var reply='no';
+console.log(reply);
+var revisedReply= reply.replace('no','yes');
+console.log(revisedReply);
 // 15
-var pas=( prompt("Enter password: ",'password123'))
-if(pas>=65 || pas<=90 || pas>=97 || pas<=122 && pas>=48 || pas<=57 ){
-    if(!(pas[0]>=48 && pas[0]<=57)){
-        if(pas.length>=6){
-            alert("you entered correct password")
-        }
-    }
+var str='1';
+console.log(str);
+var revisedStr= str.replace('1','one');
+console.log(revisedStr);
+// 16
+var y = x.replace(/a/g, "z");
+// ch 26
+// 1
+var i=20.67;
+var e=Math.round(i,2)
+console.log(e);
+// 2
+var origNum=20.67;
+var roundNum=Math.ceil(origNum)
+console.log(roundNum);
+// 3
+var origNum=20.67;
+var roundNum=Math.floor(origNum)
+console.log(roundNum);
+// 4
+var origNum=0.5;
+var roundNum=Math.floor(origNum)
+console.log(roundNum);
+// 5
+var num=Math.random();
+var num2=(num *50)+1;
+var sta2= Math.floor(num2);
+console.log(sta2);
+// 6
+var dice=Math.random();
+var num=(dice *6)+1;
+var sta= Math.floor(num);
+alert("Random value of dice is: " + sta)
+// 7
+var dice1=Math.random();
+var num1=(dice1 *2)+1;
+var sta1= Math.floor(num1);
+if(sta1==1){
+    alert("Random coin value is: " + sta1 + " and it is Heads")
 }
-else{
-    alert("please enter correct pasword which includes alphabets and num ")
+else if(sta1==2){
+    alert("Random coin value is: " + sta1 + " and it is  Tails")
 }
+// ch 28
+// 1 & 2
+var str='123';
+var int=parseInt(str);
+// 3
+var str1='123.23';
+var int=parseFloat(str1);
+// 4
+isNaN();
+parseFloat();
+parseInt();
+// 5
+var int2=123;
+var str2=int2.toString();
+console.log(str2);
+console.log(typeof str2);
+// 6
+var int2=42;
+var str2=int2.toString();
+console.log(str2);
+console.log(typeof str2);
+// 7
+var str1='13.5';
+var int5=parseInt(str1);
+console.log(int5);
+console.log(typeof int5);
+// ch 30
+// 1
+var numbr=12.3456796;
+var newNum=numbr.toFixed(4);
+console.log(numbr);
+console.log(newNum);
+console.log(typeof newNum);
 
+// 2
+var numbr_1=12.3456796;
+numbr_1= Number(numbr_1.toFixed(2))
+console.log(numbr_1);
+console.log(typeof numbr_1);
+// 3
 
+if(numbr_1.toFixed(2).length> 4){}
 
+// 4
+var number=12.3456796;
+ number=String(number.toFixed(2));
+alert(number + typeof number) 
